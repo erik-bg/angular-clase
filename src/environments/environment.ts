b77a8043cfed0982b1eab3d29ad1974e6f60cdf6
erik-bg <barreragarcia09@gmail.com>
2024-12-17 17:32:58 -0500

    Update environment.ts

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,7 +1,7 @@
 export const environment = {
   production: true,
 <<<<<<< HEAD
-  api: 'http://localhost:3000/api',
+  api: 'https://nest-clase.onrender.com/api/emails',
 =======
   api: 'https://polite-phoebe-daniel-arias-230f02a4.koyeb.app/api',
 >>>>>>> 14da75123a62ffd5d2a41a7f11051cc5277083e1
